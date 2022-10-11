80e1b1db11b489aca49188562ff0d1093862ae27
chengtsa <tsaien.arq@gmail.com>
2022-10-11 20:49:26 +0000

    arquivo gasolina.py alterado em developer, para o módulo 20

--- gasolina.py
+++ gasolina.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 
 gasolina_df = pd.read_csv('gasolina.csv', sep=',')
 
-gasolina_grafico = sns.barplot(data=gasolina_df, x="dia", y="venda", ci=None, palette="pastel")
-gasolina_grafico.set(title='Preço médio da gasolina em São Paulo', xlabel='Dia do mês de julho', ylabel='Preço médio (R$)');
+gasolina_grafico = sns.barplot(data=gasolina_df, x="dia", y="venda", ci=None, palette="bright")
+gasolina_grafico.set(title='Preço médio da gasolina em SP', xlabel='Dia do mês julho/2021', ylabel='Preço médio (em reais)');
 plt.savefig('gasolina.png')
